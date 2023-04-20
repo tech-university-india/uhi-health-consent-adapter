9b3f1e267b8e7278e40b027b8befa84d21e455c6
yashasva01 <yashasva2001@gmail.com>
2023-04-20 14:41:32 +0530

    feat: add test for util check for error

--- test/services/healthId.test.js
+++ test/services/healthId.test.js
@@ -3,43 +3,41 @@ const getToken = require('../../src/services/gateway')
 const response = require('../../src/utils/response')
 
 describe('healthId service', () => {
-	const path = '/test'
+  const path = '/test'
   const body = {}
-	const mockAccessToken = 'mockAccessToken'
-	const headers = {'content-type': 'xyz'}
+  const mockAccessToken = 'mockAccessToken'
+  const headers = { 'content-type': 'xyz' }
 
-
-	beforeEach(() => {
+  beforeEach(() => {
     jest.resetAllMocks()
     process.env.HEALTH_ID_URL = 'mockHealthIdUrl'
   })
-	afterEach(() => {
-		delete process.env.HEALTH_ID_URL
+  afterEach(() => {
+    delete process.env.HEALTH_ID_URL
   })
-
   it('should call getToken GET', async () => {
-		const method = 'GET'
-		fetch = jest.fn().mockResolvedValueOnce({
-			status: 200,
-			json: jest.fn().mockResolvedValueOnce({})
-		})
+    const method = 'GET'
+    fetch = jest.fn().mockResolvedValueOnce({
+      status: 200,
+      json: jest.fn().mockResolvedValueOnce({})
+    })
 
-		response.checkForErrorsInResponse = jest.fn().mockResolvedValueOnce({})
-		response.convertToResponseBody = jest.fn().mockResolvedValueOnce({})
+    response.checkForErrorsInResponse = jest.fn().mockResolvedValueOnce({})
+    response.convertToResponseBody = jest.fn().mockResolvedValueOnce({})
     getToken.getToken = jest.fn().mockResolvedValue(mockAccessToken)
     await healthId.healthId(path, method, headers, body)
     expect(getToken.getToken).toBeCalledTimes(1)
   })
 
-	it('should call getToken POST', async () => {
-		const method = 'POST'
-		fetch = jest.fn().mockResolvedValueOnce({
-			status: 200,
-			json: jest.fn().mockResolvedValueOnce({})
-		})
+  it('should call getToken POST', async () => {
+    const method = 'POST'
+    fetch = jest.fn().mockResolvedValueOnce({
+      status: 200,
+      json: jest.fn().mockResolvedValueOnce({})
+    })
 
-		response.checkForErrorsInResponse = jest.fn().mockResolvedValueOnce({})
-		response.convertToResponseBody = jest.fn().mockResolvedValueOnce({})
+    response.checkForErrorsInResponse = jest.fn().mockResolvedValueOnce({})
+    response.convertToResponseBody = jest.fn().mockResolvedValueOnce({})
     getToken.getToken = jest.fn().mockResolvedValue(mockAccessToken)
     await healthId.healthId(path, method, headers, body)
     expect(getToken.getToken).toBeCalledTimes(1)

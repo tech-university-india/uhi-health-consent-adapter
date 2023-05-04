6a58abfd091f1704f76ea8e09d11ba72c79ef012
SaatwikRishi <38259107+SaatwikRishi@users.noreply.github.com>
2023-05-04 12:54:13 +0530

    Chore/remove redis (#7)
    * fix: tests and mcache
    
    * fix: m-cache
    
    ---------
    
    Co-authored-by: SaatwikRishi <saatwik_s@mckinsey.com>

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken')
 const { setCache, getCache } = require('./cache')
 const Response = require('./response')
 
-let DEFAULT_EXPIRY = 60
+let DEFAULT_EXPIRY = 60 * 1000
 
 const cacheFetch = async (requestOptions, isToken) => {
-  const cacheData = await getCache(requestOptions.url)
+  const cacheData = getCache(requestOptions.url)
   if (cacheData) {
     return JSON.parse(cacheData)
   }
@@ -13,14 +13,17 @@ const cacheFetch = async (requestOptions, isToken) => {
   delete requestOptions.url
   const response = await fetch(url, requestOptions)
   await Response.checkForErrorsInResponse(response)
-
   const data = await Response.convertToResponseBody(response)
   if (isToken) {
     const { exp } = jwt.decode(data.accessToken ?? data)
-    DEFAULT_EXPIRY = exp - Math.floor(Date.now() / 1000) - 60
+    DEFAULT_EXPIRY = (exp - ((Math.floor(Date.now() / 1000) - 60))) * 1000
   }
 
-  await setCache(requestOptions.url, JSON.stringify(data.accessToken ? data : { accessToken: data }), DEFAULT_EXPIRY)
+  setCache(
+    url,
+    JSON.stringify(data.accessToken ? data : { accessToken: data }),
+    DEFAULT_EXPIRY
+  )
   return data.accessToken ? data : { accessToken: data }
 }
 

d478af31448fe441c142575d9f91761a149ffdb6
SaatwikRishi <saatwik_s@mckinsey.com>
2023-05-04 11:53:21 +0530

    opt: remove redis and add mcache

---
+++ src/utils/errors/errorHandler.js
@@ -0,0 +1,10 @@
+const { HttpError } = require('./httpError')
+
+const errorHandlerInRoute = (error, req, res) => {
+  if (error instanceof HttpError) {
+    return res.status(error.statusCode).json({ message: error.message })
+  }
+  res.status(500).json({ message: 'Internal server error' })
+}
+
+module.exports = { errorHandlerInRoute }
